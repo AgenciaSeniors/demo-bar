76723a83f84ce0551736eb54fa6cc540761e147a
AgenciaSeniors <seniors.contacto@gmail.com>
2025-12-14 00:10:59 -0500

    ok

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // --- 1. CONFIGURACIÓN ---
-const SUPABASE_URL = 'TU_URL_DE_SUPABASE';
-const SUPABASE_KEY = 'TU_KEY_ANONIMA';
+const SUPABASE_URL = 'https://qspwtmfmolvqlzsbwlzv.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzcHd0bWZtb2x2cWx6c2J3bHp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2ODY0NjAsImV4cCI6MjA4MTI2MjQ2MH0.7QNMreDfX1wgjPVS3mz0T3cl_seQRZPaUruhAHKxPbY';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 let todosLosProductos = [];
