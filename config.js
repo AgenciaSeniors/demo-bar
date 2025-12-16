a3149ec184dd526b75e2a151e06a03d84fb037bb
AgenciaSeniors <seniors.contacto@gmail.com>
2025-12-16 01:12:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     SUPABASE_URL: 'https://qspwtmfmolvqlzsbwlzv.supabase.co',
     SUPABASE_KEY: 'sb_publishable_ba5r8nJ5o49w1b9TURDLBA_EbMC_lWU',
     // Usamos el modelo más estable para evitar errores 404
-    GEMINI_KEY: 'AIzaSyAQNSLLZSNIWjAW4b9Ifke1TWT5YPtZ0cE' 
+    GEMINI_KEY: 'AIzaSyDDqPRUFilvcpVlFlMnxkYYju4RnVAFGv4' 
 };
 
 // Cliente Global de Supabase
@@ -10,3 +10,4 @@ const CONFIG = {
 //entonce NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY=sb_publishable_XtV2kYHISXME2K-STuHmdw_UUGTZyvS
 //SUPABASE_URL https://supabase.com/dashboard/project/mvtatdvpsjynvayhhksc/settings/api ====== Project URL
 const supabaseClient = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
+
